a078b77df5582264b1074b3f42bb6b1a211f9bc9
Enfu Liao <enfu.liao.cs10@nycu.edu.tw>
2023-05-05 22:07:32 +0800

    update

--- template.cpp
+++ template.cpp
@@ -1,14 +1,24 @@
 #include <bits/stdc++.h>
 #define LOCAL
 
+#define all(x) (x).begin(), (x).end()
+
 using namespace std;
+
+
+int nxt() {
+    int x;
+    cin >> x;
+    return x;
+}
+
 
 int main() {
 #ifdef LOCAL
     freopen("input.txt", "r", input);
     freopen("output.txt", "w", stdout);
 #endif
-    ios::sync_with_stdio(false);
+    ios::sync_with_stdio(0);
     cin.tie(0);
 
     return 0;
